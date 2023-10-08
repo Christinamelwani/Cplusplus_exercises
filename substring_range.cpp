53a72d06a6b5e4a06a11dcb68231414cfae949cc
christinamelwani <christinarajm@gmail.com>
2023-10-08 19:31:42 +0700

    Substring range exercise complete

--- substring_range.cpp
+++ substring_range.cpp
@@ -1,2 +1,25 @@
 #include <iostream>
 #include <string>
+
+int main(){
+    std::string str;
+    int start;
+    int end;
+
+    std::cout<<"Enter a string: "<<std::endl;
+    std::getline(std::cin, str);
+
+    std::cout<<"Enter the starting position: "<<std::endl;
+    std::cin>>start;
+
+    std::cout<<"Enter the ending position: "<<std::endl;
+    std::cin>>end;
+
+    if(end < str.length() && start < end){
+        std::cout<<"The string you entered starting from position "<<start<<" to "<<end<<" is: "<<str.substr(start, end)<<std::endl;
+    } else{
+        std::cout<<"The starting and end position are unsuitable. Please check again."<<std::endl;
+    }
+ 
+    return 0;
+}
