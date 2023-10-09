0e5fd19c6a1845cc6358f04f2c9b201f49ef06b3
christinamelwani <christinarajm@gmail.com>
2023-10-09 12:31:45 +0700

    Completed functions exercise

--- day2/functions.cpp
+++ day2/functions.cpp
@@ -1,11 +1,23 @@
 #include <iostream>
 #include <string>
 
-int main(){
+int calculateSum(int integer1, int integer2){
+    return integer1 + integer2;
+}
 
+int main(){
     int integer1;
     int integer2;
 
+    std::cout<<"Enter the first integer: "<<std::endl;
+    std::cin>>integer1;
+
+    std::cout<<"Enter the second integer: "<<std::endl;
+    std::cin>>integer2;
+
+    int result = calculateSum(integer1, integer2);
+
+    std::cout<<"The sum of "<<integer1<<" and "<<integer2<<" is "<<result<<std::endl;
     
     return 0;
 }
