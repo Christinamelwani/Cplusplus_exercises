d8e10b90901806f4cd8d5527ff54eaa75ad862ac
christinamelwani <christinarajm@gmail.com>
2023-10-10 13:32:56 +0700

    Updated sandbox

--- sandbox.cpp
+++ sandbox.cpp
@@ -1,9 +1,18 @@
-#include <stdio.h>
-#include <math.h>
-int main()
-{
-printf("%f\n",sqrt(10.0));
-printf("%f\n",sin(0.0));
-printf("%f\n",tan(0.0));
-return 0;
+#include <iostream>
+
+using namespace std;
+
+void sum(int arr1[], int arr2[]){
+    int temp[5];
+    for(int i=0; i<5; i++){
+        temp[i] = arr1[i]+arr2[i];
+        cout<<temp[i]<<endl;
+    }
+}
+
+int main(){
+    int a[5] = {10, 20, 30, 40 ,50};
+    int b[5] = {1, 2, 3, 4, 5};
+    sum(a, b);
+    return 0;
 }
